482c1168b1f97a94f95e7ce913c79e58f9fa3ab0
wickx98 <wickx98@gmail.comm>
2025-06-09 17:54:28 +0530

    Revert "update"
    This reverts commit 37d47f2e98c4a20febe1fac1fb8ab1249bdce631.

--- src/views/Presentation/PresentationView.vue
+++ src/views/Presentation/PresentationView.vue
@@ -62,10 +62,11 @@ onUnmounted(() => {
               class="text-white pt-3 mt-n5 me-2"
               :style="{ display: 'inline-block ' }"
             >
-              UrbanPulse Lanka
+              Material Kit 2
             </h1>
             <p class="lead text-white px-5 mt-3" :style="{ fontWeight: '500' }">
-              Welcome to UrbanPulse Lanka — your digital gateway to a smarter, cleaner, and more connected urban future. Our mission is to highlight local challenges and promote sustainable, tech-driven solutions that enhance everyday city living.
+              Start the Development With Bootstrap 5 Design System inspired by
+              Material Design.
             </p>
           </div>
         </div>
@@ -74,11 +75,11 @@ onUnmounted(() => {
   </Header>
 
   <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
-    <!-- <PresentationCounter />
+    <PresentationCounter />
     <PresentationInformation />
     <PresentationExample :data="data" />
     <PresentationPages />
-    <BuiltByDevelopers /> -->
+    <BuiltByDevelopers />
 
     <div class="container">
       <div class="row">
@@ -261,7 +262,7 @@ onUnmounted(() => {
         </div>
       </div>
     </div>
-    <!-- <div class="py-5">
+    <div class="py-5">
       <div class="container">
         <div class="row">
           <div class="col-lg-5 ms-auto">
@@ -290,7 +291,7 @@ onUnmounted(() => {
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
   <DefaultFooter />
 </template>
--- src/views/Presentation/Sections/PresentationCounter.vue
+++ src/views/Presentation/Sections/PresentationCounter.vue
@@ -3,16 +3,17 @@ import DefaultCounterCard from "../../../examples/cards/counterCards/DefaultCoun
 </script>
 
 <template>
-  <section class="pt-4 pb-4" id="count-stats">
+  <section class="pt-3 pb-4" id="count-stats">
     <div class="container">
       <div class="row">
-        <div class="col-lg-9 z-index-2 border-radius-xl mx-auto py-4">
+        <div class="col-lg-9 z-index-2 border-radius-xl mx-auto py-3">
           <div class="row">
             <div class="col-md-4 position-relative">
               <DefaultCounterCard
                 color="success"
-                title="Traffic Management"
-                description="Transforming urban mobility through intelligent traffic lights, real-time transit updates, and optimized public transport routes. Our goal is to reduce congestion, travel time, and pollution for a more efficient commute."
+                title="Coded Elements"
+                description="From buttons, to inputs, navbars, alerts or cards, you are
+                  covered"
                 :count="70"
                 suffix="+"
                 :duration="3000"
@@ -22,8 +23,9 @@ import DefaultCounterCard from "../../../examples/cards/counterCards/DefaultCoun
             <div class="col-md-4 position-relative">
               <DefaultCounterCard
                 color="success"
-                title="Energy Efficiency"
-                description="Promoting sustainable energy practices by encouraging solar power, smart meters, and energy-conscious living. Together, we can lower carbon footprints and create more resilient power systems for our communities."
+                title="Design Blocks"
+                description="Mix the sections, change the colors and unleash your
+                  creativity"
                 :count="15"
                 suffix="+"
                 :duration="3000"
@@ -33,17 +35,9 @@ import DefaultCounterCard from "../../../examples/cards/counterCards/DefaultCoun
             <div class="col-md-4">
               <DefaultCounterCard
                 color="success"
-                title="Waste Management"
-                description="Reimagining waste disposal with smart bins, community recycling programs, and waste-to-energy solutions. Proper management not only enhances public health but also protects the environment."
-                :count="4"
-                :duration="3000"
-              />
-            </div>
-            <div class="col-md-4">
-              <DefaultCounterCard
-                color="success"
-                title="Urban Greening"
-                description="Enhancing green spaces through tree planting, vertical gardens, and eco-friendly urban planning. A greener city means cooler temperatures, cleaner air, and improved well-being for everyone."
+                title="Pages"
+                description="Save 3-4 weeks of work when you use our pre-made pages for
+                  your website"
                 :count="4"
                 :duration="3000"
               />
